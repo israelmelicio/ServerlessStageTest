beaefb9a1ab808a6e6c9b7cd717b1f2b2052ff47
Israel Melicio <israel@macwingo.com.br>
2019-09-07 22:43:27 -0300

    StageTest v1.0.0

--- handler.js
+++ handler.js
@@ -4,11 +4,7 @@ module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: `My environment is ${process.env.MY_ENVIRONMENT}`
     }, null, 2),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
